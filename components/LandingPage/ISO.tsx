5815f080bc8ffdc204e209b759a6162a1dfe1d4b
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-08-16 14:59:52 +0100

    ui update

--- components/LandingPage/ISO.tsx
+++ components/LandingPage/ISO.tsx
@@ -9,7 +9,7 @@ export default function ISO() {
       <h2 className="text-6xl font-semibold">
         Introducing Solar On - chain (ISO)
       </h2>
-      <div className="w-10/12 mx-auto mt-24 mb-40 flex gap-12">
+      <div className="w-10/12 mx-auto mt-24 mb-40 flex gap-8">
         <FeatureCard title="SunDAO Liquidity" description="Purchase SunDAO tokens, contribute to the pool, and invest in solar projects worldwide" imgUrl={Image2} />
         <FeatureCard title="Grow Your Portfolio with SunDAO" description="SunDAO acquires yield-generating commercial solar projects, delivering value directly back to token holders." imgUrl={Image3} />
         <FeatureCard title="Transact Borderless with Ease" description="Access global solar projects as easily as trading tokens. Say goodbye to paperwork—blockchain makes it seamless." imgUrl={Image1} />
@@ -20,7 +20,7 @@ export default function ISO() {
 
 function FeatureCard({ title, description, imgUrl }: { title: string, description: string, imgUrl: StaticImageData }) {
   return (
-    <div className="flex flex-col items-center space-y-4">
+    <div className="flex-1 flex flex-col items-center space-y-4">
       <Image src={imgUrl} alt="feature image" />
       <h2 className="font-semibold text-xl">{title}</h2>
       <p className="w-3/4">{description}</p>
