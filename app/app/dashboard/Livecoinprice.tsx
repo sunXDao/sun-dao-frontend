6cdfd1c8502a8108f34ec33b157680bbdddd87c8
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-09-04 00:45:20 +0100

    bug fix

--- app/app/dashboard/Livecoinprice.tsx
+++ app/app/dashboard/Livecoinprice.tsx
@@ -8,36 +8,35 @@ export default function Livecoinprice() {
   const [ethData, setEthData] = useState<any>(null);
   const [error, setError] = useState<any>(null);
 
-  // const fetchPrice = async () => {
-  //   try {
-  //     const response = await getCoinData()
-  //     const eth = response.data.find((coin: any) => coin.symbol === 'ETH');
-  //     console.log(ethData?.quote?.USD?.price.toFixed(2));
-  //     setEthData(eth);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+  const fetchPrice = async () => {
+    try {
+      const response = await getCoinData()
+      const eth = response.data.find((coin: any) => coin.symbol === 'ETH');
+      console.log(ethData?.quote?.USD?.price.toFixed(2));
+      setEthData(eth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchPrice();
-  //   const intervalId = setInterval(fetchPrice, 180000);
+  useEffect(() => {
+    fetchPrice();
+    const intervalId = setInterval(fetchPrice, 180000);
 
-  //   return () => clearInterval(intervalId);
-  // }, []);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <section className="flex gap-20 items-center">
       <TokenInfo tokenPrice={0.01467} tokenProgressPercentage={0.53} tokenSymbol="SVL" />
-      <TokenInfo tokenPrice={0.01467} tokenProgressPercentage={0.53} tokenSymbol="SVL" />
-      {/* {
+      {/* <TokenInfo tokenPrice={0.01467} tokenProgressPercentage={0.53} tokenSymbol="SVL" /> */}
+      {
         ethData ?
           <TokenInfo
             tokenPrice={ethData?.quote?.USD?.price.toFixed(2)}
-            tokenProgress="loss"
             tokenProgressPercentage={ethData?.quote?.USD?.percent_change_24h.toFixed(2)}
             tokenSymbol={ethData?.symbol}
           /> : <p>Loading...</p>
-      } */}
+      }
     </section>
   )
 }
