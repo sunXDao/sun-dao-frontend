2c6f289af0cb83c9295c6a7b29b5013bfcc84487
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-09-04 00:42:52 +0100

    env config

--- utils/data.ts
+++ utils/data.ts
@@ -4,12 +4,11 @@ export const getCoinData = async () => {
   try {
     const res = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
       headers: {
-        'X-CMC_PRO_API_KEY': '3d010422-112a-4949-80f6-ceaa7661eaf4',
+        'X-CMC_PRO_API_KEY': process.env.API_KEY || "" as string,
       },
     });
     const data = await res.json();
     return data
-    // setPrice(response.data.data.BTC.quote.USD.price);
   } catch (err) {
     console.error(err);
   }
